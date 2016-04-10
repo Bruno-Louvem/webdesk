57b89bbb6b1a2174d9e848dd6314629ce68ec175
Bruno <btlouvem@gmail.com>
2016-04-10 17:21:26 +0000

    Primeiro commit

---
+++ global/services.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'services' => array(
+//        'demo.custom_service' => array(
+//            'class'=>'DemoApp\\Services\\Custom',
+//            'strategy'=>'instance'
+//        ),
+    )
+);
